8c453bfbcc362ed8002a1723be2dee9f930d5f10
Lienne Sethna <30530947+lsethna@users.noreply.github.com>
2020-12-17 11:44:41 -0500

    Merge all WRTDS discharge files into one master discharge file

--- Merge Site Discharge/Merge site discharge_LRS_14Dec20.R
+++ Merge Site Discharge/Merge site discharge_LRS_14Dec20.R
@@ -8,7 +8,12 @@ require(stringr)
 require(lubridate)
 require(reshape)
 require(gtools)
+require(plyr)
 require(dplyr)
+require(tidyr)
+require(data.table)
+require(dataRetrieval)
+
 
 #get folder URL from google drive with discharge data
 folder_url = "https://drive.google.com/drive/folders/1PSa4QDG1FJyuzvZC6lbLPWgO7VgMX4ef"
@@ -19,7 +24,7 @@ folder = drive_get(as_id(folder_url))
 #get list of csv files from folder
 csv_files = drive_ls(folder, type="csv")
 
-#get just site names from csv file names
+#get just site names from csv file name
 csv_files$site = str_extract(csv_files$name, pattern="(?<=Copy of ).*(?=.csv)")
 csv_files$site = strsplit(csv_files$site, "_")
 library(dplyr)
@@ -34,3 +39,40 @@ site_names =
                 unite("site", ...1:...4,sep="_",na.rm=T)
 #insert site names back into csv_files dataframe
 csv_files$site = site_names
+
+#extract only WRTDS discharge files
+WRTDS_discharge = csv_files[csv_files$name %like% "Q_WRTDS.csv",]
+
+#check working directory where files will be stored locally; separate folder within project folder
+getwd()
+#"L:/GitHub/SiSyn/Merge Site Discharge"
+setwd("L:/GitHub/SiSyn/Merge Site Discharge/discharge files")
+
+#download each file to the working directory; files are saved locally
+for (i in 1:length(WRTDS_discharge$drive_resource)) {
+  drive_download(WRTDS_discharge$drive_resource[i],  overwrite=T)
+}
+
+#add column for site name
+#loop through each downloaded csv file and add appropriate site name
+discharge_files = list.files(path="L:/GitHub/SiSyn/Merge Site Discharge/discharge files")
+
+data_list = list()
+
+for (i in 1:length(discharge_files)) {
+  site = substr(discharge_files[i],start=9,stop=nchar(discharge_files[i])-12)
+  
+  file_name = discharge_files[i]
+  d = fread(file_name, sep=",")
+  d$site.name = site
+  
+  data_list[[i]] = d
+}
+
+#use rbind to concatenate each new discharge file
+#should have 3 columns: date, site, discharge
+#all_discharge = rbindlist(data_list)
+all_discharge = ldply(data_list, data.frame)
+
+#which 
+discharge_sites = data.frame("site"=unique(all_discharge$site.name))
