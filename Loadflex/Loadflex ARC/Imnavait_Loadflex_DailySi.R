9e11c3f92f14aa42adaa1b41efbedbae7b6f74a4
Lienne Sethna <30530947+lsethna@users.noreply.github.com>
2021-05-18 15:30:28 -0400

    Updates to Imnavait discharge comparison

--- Loadflex/Loadflex ARC/Imnavait_Loadflex_DailySi.R
+++ Loadflex/Loadflex ARC/Imnavait_Loadflex_DailySi.R
@@ -14,13 +14,16 @@ head(Imnavait_Weir_Si)
 Imnavait_int = merge(Imnavait_Weir_Q, Imnavait_Weir_Si)
 head(Imnavait_int)
 
+#remove interpolation data pre 2002
+Imnavait_int = subset(Imnavait_int, Imnavait_int$Date >= "2002-01-01")
+
 #duplicated days and negative/0 values not permitted for Loadflex
 #use average daily Si for interpolation data
 Imnavait_int = 
   Imnavait_int %>%
   group_by(Date) %>%
   summarize(
-    Q_cms = mean(Q),
+    Q_cms = mean(Q_cms),
     Si = mean(Si)
   )
 head(Imnavait_int)
@@ -30,14 +33,21 @@ ggplot(Imnavait_int, aes(x=Q_cms, y=Si))+
   scale_x_continuous(trans="log10")+
   theme_bw(base_size=24)
 
+#calculate instantaneous load from measured data
+Imnavait_int$load = (Imnavait_int$Q_cms*1000) * Imnavait_int$Si * (86400/1e6)
+
 #metadata description of the dataset and desired output
 meta = metadata(constituent="Si", flow="Q_cms", dates="Date",
-                conc.units="mg L^-1", flow.units="cms", load.units="kg",
+                conc.units="mg L^-1", flow.units="m^3 s^-1", load.units="kg",
                 load.rate.units="kg d^-1", site.name="Imnavait_Weir",
                 consti.name="Dissolved SiO2")
 
 #run composite model using rloadest package
 library(rloadest)
+#remove negative values from interpolation data
+Imnavait_int$Q_cms = ifelse(Imnavait_int$Q_cms<=0,NA,Imnavait_int$Q_cms)
+Imnavait_int = na.omit(Imnavait_int)
+
 Imnavait_lr = loadReg2(loadReg(Si~Q_cms, data=Imnavait_int,
                               flow="Q_cms", dates="Date", conc.units="mg/L", load.units="kg"))
 Imnavait_lc = loadComp(reg.model=Imnavait_lr, interp.format="conc", interp.data=Imnavait_int)
@@ -45,8 +55,12 @@ Imnavait_lc = loadComp(reg.model=Imnavait_lr, interp.format="conc", interp.data=
 getFittedModel(Imnavait_lc)
 ggplot2::qplot(x=Date, y=Resid, data=getResiduals(Imnavait_lc, newdata=Imnavait_int))
 
+#run rectangular interpolation model
+Imnavait_li = loadInterp(interp.format="conc", interp.function=linearInterpolation,
+                         data=Imnavait_int, metadata=meta)
+
 #generate point predictions from daily Q values
-preds_lc = predictSolute(Imnavait_lc,"flux",Imnavait_Weir_Q,se.pred=T,date=T)
+preds_lc = predictSolute(Imnavait_li,"flux",Imnavait_Weir_Q,se.pred=T,date=T)
 head(preds_lc)
 
 names(preds_lc)[names(preds_lc)=="fit"] = "Si_load_kg.day"
@@ -65,8 +79,14 @@ Imnavait_loads = merge(preds_lc, Imnavait_Weir_Q, by=c("Date"))
 head(Imnavait_loads)
 
 #plot by date, each line is year
-ggplot(preds_lc)+
-  geom_line(aes(x=Date, y=Si_load_kg.day))
+Imnavait_pred_loads = merge(Imnavait_int, Imnavait_loads, by=c("Date","Q_cms"), all=T)
+
+ggplot(Imnavait_pred_loads)+
+  geom_point(aes(x=Date, y=load))+
+  geom_line(aes(x=Date, y=Si_load_kg.day), color="blue")+
+  ylab("Si load (kg/day)")+
+  xlab("")+
+  theme_classic(base_size=18)
 
 #write csv
 write.csv(Imnavait_loads, file="Imnavait_dailySi_Loadflex.csv")
--- Merge Site Discharge/ARC_similarQsites.R
+++ Merge Site Discharge/ARC_similarQsites.R
@@ -54,3 +54,56 @@ ggplot(annual_Q_median_0, aes(x=yday, y=meanQ))+
   ylab("Mean daily discharge (cms)")+
   facet_wrap(~site.name, scales="free")+
   theme_bw()
+
+
+#remove zeros from discharge files, get median Q flow values
+discharge_abvzero = data.frame(site.name=discharge_nodate$site.name,
+                               Q=ifelse(discharge_nodate$Q==0, NA, discharge_nodate$Q))
+discharge_abvzero = na.omit(discharge_abvzero)
+
+ggplot(discharge_abvzero, aes(x=site.name, y=Q))+
+  geom_boxplot(outlier.shape=NA)+
+  scale_y_continuous(limits=c(0,1))+
+  theme_bw()+
+  theme(axis.text.x=element_text(angle=90))
+
+
+median_abvzero_Q = 
+  discharge_abvzero %>%
+  group_by(site.name) %>%
+  summarize(
+    medianQ = median(Q)
+  )
+
+#Imnavait Weir median Q (during flow period) = 1.6e-2
+#subset sites with median Q with similar median Q
+median_abvzero_Q = subset(median_abvzero_Q, median_abvzero_Q$medianQ<=0.1)
+abvzero_Q_sites = c(median_abvzero_Q$site.name,"ws3","ws6","ws7","ws8","ws9")
+median_abvzero_sites = all_discharge_NAomit[all_discharge_NAomit$site.name %in% abvzero_Q_sites,]
+length(unique(median_abvzero_sites$site.name))
+
+#plot hydrograph of all sites with median flow values similar to Imnavait
+median_abvzero_sites$Date = as.Date(median_abvzero_sites$Date)
+median_abvzero_sites$yday = yday(median_abvzero_sites$Date)
+#remove Date; getting error with dplyr functions
+median_abvzero_sites = subset(median_abvzero_sites, select=-c(Date))
+
+annual_Q_median_abv0 =
+  median_abvzero_sites %>%
+  group_by(site.name, yday) %>%
+  summarize(
+    meanQ = mean(Q)
+  )
+
+ggplot(annual_Q_median_abv0, aes(x=yday, y=meanQ))+
+  geom_line()+
+  geom_hline(
+    data=annual_Q_median_abv0 %>%
+      group_by(site.name) %>%
+      summarize(line=mean(median(meanQ))),
+    mapping=aes(yintercept=line), color="red", linetype="dashed"
+  )+
+  xlab("Day of year")+
+  ylab("Mean daily discharge (cms)")+
+  facet_wrap(~site.name, scales="free")+
+  theme_bw(base_size=15)
