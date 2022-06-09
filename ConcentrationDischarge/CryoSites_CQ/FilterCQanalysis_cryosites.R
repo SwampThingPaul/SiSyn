87bd1e0e290c986ec59ca4abdf6fb6686f61eea4
Lienne Sethna <30530947+lsethna@users.noreply.github.com>
2022-06-09 17:18:01 -0500

    Updates to long-term CQ analysis

--- ConcentrationDischarge/CryoSites_CQ/FilterCQanalysis_cryosites.R
+++ ConcentrationDischarge/CryoSites_CQ/FilterCQanalysis_cryosites.R
@@ -3,30 +3,18 @@ library(ggplot2)
 
 #check if all cryo sites are in CQ MBLM analysis file
 cryo_sites = unique(ObsDSi_sumstats_6_2_22$site)
-length(intersect(cryo_sites,LTERsites_CQmblm_9Jun22$site.name))
-setdiff(cryo_sites,LTERsites_CQmblm_9Jun22$site.name)
+length(intersect(cryo_sites,AnnualCQslopes_mlbmresults$site.name))
+setdiff(cryo_sites,AnnualCQslopes_mlbmresults$site.name)
 
 #filter mblm results to cryo sites alone
-cryo_mblm = subset(LTERsites_CQmblm_9Jun22,LTERsites_CQmblm_9Jun22$site.name%in%cryo_sites)
-
-#repeat for annual CQ slope data
-cryo_annualCQ = subset(LongTerm_annualCQ_slopes,LongTerm_annualCQ_slopes$site.name%in%cryo_sites)
-#remove NA p values?
-cryo_annualCQ = na.omit(cryo_annualCQ)
-
-#get stats on number of significant/non-significant annual relationships
-cryo_annualCQ_stats =
-  cryo_annualCQ %>%
-  group_by(site.name,sig)%>%
-  summarize(
-    n=n()
-  )
+cryo_mblm = subset(AnnualCQslopes_mlbmresults,AnnualCQslopes_mlbmresults$site.name%in%cryo_sites)
 
 #plot annual CQ slope for each cryo site
-head(cryo_annualCQ)
-ggplot(cryo_annualCQ, aes(x=year, y=slope, color=sig))+
+head(cryo_mblm,aes(x=year, y=slope, color=sig))
+ggplot(cryo_mblm)+
   geom_point()+
   scale_color_manual(values=c("gray","black"))+
+  geom_abline(yintercept=mblm_intercept,slope=mblm_slope)+
   scale_x_continuous(limits=c(1980,2020))+
   geom_hline(yintercept=0, lty="dashed")+
   facet_wrap(~site.name,scales="free_y")+
