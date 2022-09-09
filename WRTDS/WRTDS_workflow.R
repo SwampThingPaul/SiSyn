8bf6cbce93295dda4ccd92e90f3ccdcca0e73509
njlyon0 <nicholasjlyon@gmail.com>
2022-09-09 15:39:34 -0400

    Renamed the log file as `ref_table` and commented-out my environment clearing steps

--- WRTDS/WRTDS_workflow.R
+++ WRTDS/WRTDS_workflow.R
@@ -11,7 +11,7 @@
 librarian::shelf(tidyverse, googledrive, lubridate, reshape, gtools)
 
 # Clear environment
-rm(list = ls())
+# rm(list = ls())
 
 # Authenticate with GoogleDrive so we can down/upload directly
 my_email <- "lyon@nceas.ucsb.edu"
@@ -48,7 +48,7 @@ disc_log <- read.csv(file = file.path("WRTDS Content", disc_files[2,1]))
 chem_main <- read.csv(file = file.path("WRTDS Content", chem_files[1,1]))
 
 # Clean up the environment before continuing
-rm(list = setdiff(ls(), c("disc_main", "disc_log", "chem_main")))
+# rm(list = setdiff(ls(), c("disc_main", "disc_log", "chem_main")))
 ## Above line removes any objects *other* than those specified
 
 ## ---------------------------------------------- ##
@@ -59,10 +59,24 @@ disc_v2 <- disc_main %>%
   # Drop row number column
   dplyr::select(-X) %>%
   # Rename site column as it appears in the discharge log file
-  dplyr::rename(Stream = site.name)
+  dplyr::rename(Stream = site.name) %>%
+  # Convert date to true date format
+  dplyr::mutate(Date = as.Date(Date, "%Y-%m-%d"))
 
 # Check that out
 dplyr::glimpse(disc_v2)
+
+# Wrangle the discharge log information as well
+ref_table <- disc_log %>%
+  # Rename file name columns
+  dplyr::rename(files = DischargeFileName) %>%
+  # Crop down to only needed columns
+  dplyr::select(files, Stream, Units) %>%
+  # Keep only streams in the discharge dataset
+  dplyr::filter(Stream %in% disc_v2$Stream)
+
+# Check it
+dplyr::glimpse(ref_table)
 
 # Clean up the chemistry data
 chem_v2 <- chem_main %>%
@@ -78,8 +92,10 @@ chem_v2 <- chem_main %>%
     variable_simp == "DSi" ~ (((value / 10^6) * 28.0855) * 1000),
     variable_simp == "NOx" ~ (((value / 10^6) * 14.0067) * 1000),
     variable_simp == "NH4" ~ ((value / 10^6) * 14.0067) * 1000 )) %>%
-  # Rename date column
-  dplyr::rename(Date = Sampling.Date)
+  # Rename some columns
+  dplyr::rename(Stream = Site.Stream.Name, Date = Sampling.Date) %>%
+  # Convert date to true date format
+  dplyr::mutate(Date = as.Date(Date, "%Y-%m-%d"))
   # For Andrews sites, remove pree-1982
 
 
