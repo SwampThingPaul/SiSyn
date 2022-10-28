ae35b0684bb37835d683a91aec3dcd57a931590d
njlyon0 <nicholasjlyon@gmail.com>
2022-10-28 11:57:24 -0400

    Tweaked results script to also anticipate, flatten, and re-export the bootstrap outputs

--- WRTDS/WRTDS_step-4_results_report.R
+++ WRTDS/WRTDS_step-4_results_report.R
@@ -19,6 +19,7 @@ rm(list = ls())
 
 # Create a new folder for saving temporary results
 dir.create(path = file.path(path, "WRTDS Results"), showWarnings = F)
+dir.create(path = file.path(path, "WRTDS Bootstrap Results"), showWarnings = F)
 
 # Download the reference table object
 ref_table <- read.csv(file = file.path(path, "WRTDS Source Files", 
@@ -36,7 +37,7 @@ dest_url <- googledrive::as_id("https://drive.google.com/drive/folders/1842KSgp4
 googledrive::drive_ls(path = dest_url)
 
 ## ---------------------------------------------- ##
-          # Identify WRTDS Outputs ----
+            # Identify WRTDS Outputs ----
 ## ---------------------------------------------- ##
 
 # List all files in "WRTDS Outputs"
@@ -123,7 +124,7 @@ names(out_list)
 rm(list = setdiff(ls(), c("path", "dest_url", "out_list", "ref_table")))
 
 ## ---------------------------------------------- ##
-          # Necessary Post Processing ----
+             # Process WRTDS Outputs ----
 ## ---------------------------------------------- ##
 
 # Handle trends table
@@ -188,11 +189,7 @@ results_table <- out_list[["ResultsTable_GFN_WRTDS.csv"]] %>%
 # Glimpse this as well
 dplyr::glimpse(results_table)
 
-## ---------------------------------------------- ##
-                # Export Products ----
-## ---------------------------------------------- ##
-
-# Combine files into a list
+# Combine processed files into a list
 export_list <- list("TrendsTable_GFN_WRTDS.csv" = trends_table,
                     "GFN_WRTDS.csv" = gfn,
                     "Monthly_GFN_WRTDS.csv" = monthly,
@@ -213,9 +210,10 @@ for(name in names(export_list)){
   write.csv(x = datum, na = "", row.names = F, file = report_file)
   
   # Upload that object to GoogleDrive
-  googledrive::drive_upload(media = report_file, overwrite = T, path = dest_url)
-  
-}
+  googledrive::drive_upload(media = report_file, overwrite = T, path = dest_url) }
+
+# Clear environment of everything but the filepath, destination URL, and ref_table
+rm(list = setdiff(ls(), c("path", "dest_url", "ref_table")))
 
 ## ---------------------------------------------- ##
             # Export PDF Reports ----
@@ -250,5 +248,139 @@ for(report in unique(pdf_outs$file_name)){
                             path = googledrive::as_id("https://drive.google.com/drive/folders/1ZG5DnW_fu65bmCgh0GnCYK89QaT9n3Ea"))
   
 }
+
+# Clear environment of everything but the filepath, destination URL, and ref_table
+rm(list = setdiff(ls(), c("path", "dest_url", "ref_table")))
+
+## ---------------------------------------------- ##
+         # Identify Bootstrap Outputs ----
+## ---------------------------------------------- ##
+
+# List all files in "WRTDS Outputs"
+boot_outs_v0 <- dir(path = file.path(path, "WRTDS Bootstrap Outputs"))
+
+# Do some useful processing of that object
+boot_outs <- data.frame("file_name" = boot_outs_v0) %>%
+  # Split LTER off the file name
+  tidyr::separate(col = file_name, into = c("LTER", "other_content"),
+                  sep = "__", remove = FALSE, fill = "right", extra = "merge") %>%
+  # Separate the remaining content further
+  tidyr::separate(col = other_content, into = c("stream", "chemical", "data_type"),
+                  sep = "_", remove = TRUE, fill = "right", extra = "merge") %>%
+  # Recreate the "Stream_Element_ID" column
+  dplyr::mutate(Stream_Element_ID = paste0(LTER, "__", stream, "_", chemical))
+
+# Glimpse it
+dplyr::glimpse(boot_outs)
+
+# Create an empty list
+boot_out_list <- list()
+
+# Define the types of output file suffixes that are allowed
+(boot_out_types <- unique(boot_outs$data_type))
+
+# For each data type...
+for(type in boot_out_types){
+  
+  # Return processing message
+  message("Processing ", type, " outputs")
+  
+  # Identify all files of that type
+  file_set <- boot_outs %>%
+    dplyr::filter(data_type == type) %>%
+    dplyr::pull(var = file_name)
+  
+  # Make a counter set to 1
+  k <- 1
+  
+  # Make an empty list
+  boot_sub_list <- list()
+  
+  # Read them all in!
+  for(file in file_set){
+    
+    # Read in CSV and add it to the list
+    boot_datum <- read.csv(file = file.path(path, "WRTDS Bootstrap Outputs", file))
+    
+    # Add it to the list
+    boot_sub_list[[paste0(type, "_", k)]] <- boot_datum %>%
+      # Add a column for the name of the file
+      dplyr::mutate(file_name = file, .before = dplyr::everything())
+    
+    # Advance counter
+    k <- k + 1
+  }
+  
+  # Once all files of that type are retrieved, unlist the sub_list!
+  boot_type_df <- boot_sub_list %>%
+    # Actual unlisting of the list
+    purrr::map_dfr(.f = dplyr::select, dplyr::everything()) %>%
+    # Bring in other desired columns
+    dplyr::left_join(y = boot_outs, by = "file_name") %>%
+    # Drop the redundant data_type column
+    dplyr::select(-data_type) %>%
+    # Relocate other joined columns to front
+    dplyr::relocate(Stream_Element_ID, LTER, stream, chemical,
+                    .after = file_name)
+  
+  # Add this dataframe to the output list
+  boot_out_list[[type]] <- boot_type_df
+  
+  # Completion message
+  message("Completed processing ", type, " outputs")
+}
+
+# Check the structure of the whole output list
+str(boot_out_list)
+names(boot_out_list)
+
+# Clear environment of everything but the filepath, destination URL, boot_out_list, & ref_table
+rm(list = setdiff(ls(), c("path", "dest_url", "boot_out_list", "ref_table")))
+
+## ---------------------------------------------- ##
+          # Process Bootstrap Outputs ----
+## ---------------------------------------------- ##
+
+# Bootstraps
+boots_gfn <- boot_out_list[["EGRETCi_GFN_bootstraps.csv"]]
+
+# Glimpse it
+dplyr::glimpse(boots_gfn)
+
+# Grab trends
+boots_trends <- boot_out_list[["EGRETCi_GFN_Trend.csv"]]
+
+# Glimpse it
+dplyr::glimpse(boots_trends)
+
+# Grab final output: pairs
+boots_pairs <- boot_out_list[["ListPairs_GFN_WRTDS.csv"]]
+
+# Glimpse it
+dplyr::glimpse(boots_pairs)
+
+# Combine processed files into a list
+boot_export_list <- list("EGRETCi_GFN_bootstraps.csv" = boots_gfn,
+                         "EGRETCi_GFN_Trend.csv" = boots_trends,
+                         "ListPairs_GFN_WRTDS.csv" = boots_pairs)
+
+# Loop across the list to export locally and to GoogleDrive
+## Note that the "GFN_WRTDS.csv" file is *huge* so it takes a few seconds to upload
+for(name in names(boot_export_list)){
+  
+  # Rip out that dataframe
+  boot_datum <- boot_export_list[[name]]
+  
+  # Define name for this file
+  boot_report_file <- file.path(path, "WRTDS Bootstrap Results", paste0("Bootstrap_Full_Results_", name))
+  
+  # Write this CSV out
+  write.csv(x = boot_datum, na = "", row.names = F, file = boot_report_file)
+  
+  # Upload that object to GoogleDrive
+  googledrive::drive_upload(media = boot_report_file, overwrite = T, path = dest_url) }
+
+# Clear environment of everything but the filepath, destination URL, and ref_table
+rm(list = setdiff(ls(), c("path", "dest_url", "ref_table")))
 
 # End ----
