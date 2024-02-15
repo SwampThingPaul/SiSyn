c09b706f9974f6ece395f413a26e5f1d369bfcac
Lienne Sethna <30530947+lsethna@users.noreply.github.com>
2024-02-15 12:17:43 -0600

    QA/QC for USGS geogenic solute chemistry

---
+++ DataCleaning/USGS_geogenic_QAQC.R
@@ -0,0 +1,94 @@
+## -------------------------------------------------------- ##
+# QA/QC of USGS geogenic solute data
+## -------------------------------------------------------- ##
+# Script Authors: Lienne Sethna
+
+# PURPOSE:
+## Check data for outliers and perform QA/QC checks
+
+# Load libraries needed for script
+# install.packages("librarian")
+librarian::shelf(tidyverse, googledrive, purrr, readxl, supportR,pracma)
+
+# Clear environment
+rm(list = ls())
+
+## ----------------------------------- ##
+# Housekeeping ----
+## ----------------------------------- ##
+
+# Identify URLs of Drive folders with needed data
+masterdata_url <- "https://drive.google.com/drive/u/1/folders/1hbkUsTdo4WAEUnlPReOUuXdeeXm92mg-"
+
+# Identify needed data in the Drive
+wanted_files <- googledrive::drive_ls(path = masterdata_url) %>%
+  # Filter to only needed files
+  dplyr::filter(name %in% c("USGS_geogenic_solutes.csv"))
+
+# Check those files
+wanted_files
+
+#check working directory, set to GitHub folder
+getwd()
+setwd("C:/Users/lsethna/Documents/GitHub/SiSyn/DataCleaning")
+
+# Create a folder to download data into
+dir.create(path = file.path("raw_data"), showWarnings = F)
+
+# Download that data
+purrr::walk2(.x = wanted_files$name, .y = wanted_files$id,
+             .f = ~ googledrive::drive_download(file = .y, overwrite = T,
+                                                path = file.path("raw_data", .x)))
+
+## ----------------------------------- ##
+# Read in Data ----
+## ----------------------------------- ##
+
+# Read in reference table
+USGS_dat <-read.csv("raw_data/USGS_geogenic_solutes.csv") %>%
+  select(!X) #remove "header" row
+
+# Check the structure of that
+dplyr::glimpse(USGS_dat)
+length(unique(USGS_dat$Stream_Name)) #number of unique sites
+length(unique(USGS_dat$solutes)) #number of solutes
+
+#fix date format
+USGS_dat$date = as.Date(USGS_dat$date)
+
+## ----------------------------------- ##
+## Check number of solutes per site --
+## ----------------------------------- ##
+
+site_summary <- USGS_dat %>%
+  group_by(Stream_Name, solutes) %>%
+  summarize(n=n())
+
+## ----------------------------------- ##
+## Plot solutes to check for outliers  ## 
+## ----------------------------------- ##
+
+#create PDF with page for each site
+sites <- unique(USGS_dat$Stream_Name)
+
+#pdf size 7" x 6"
+pdf('USGS_geogenic_plots.pdf',width=7,height=6)
+#loop to create individual pre/post plots
+for (i in 1:length(sites)) {
+  q <- subset(USGS_dat,USGS_dat$Stream_Name==sites[i])
+  
+  p <- ggplot(q, aes(x=date,y=value))+
+    geom_point()+
+    labs(title=sites[i])+
+    theme_classic()+
+    facet_wrap(~solutes,scales="free_y")
+    
+  print(p)
+}
+dev.off()
+
+## ---------------------------------------------- ##
+## Use hampel filter to detect and remove outliers
+## ---------------------------------------------- ##
+
+site_dat <- subset(USGS_dat)
